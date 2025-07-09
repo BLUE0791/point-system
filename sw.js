2619f3d75ef2ac51e6df9c1d4df9f10f3f691b97
BLUE0791 <bluelinsihan@gmail.com>
2025-07-09 21:34:51 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+{
+    "name": "积分奖励系统",
+    "short_name": "积分系统",
+    "icons": [
+        {
+            "src": "icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+        {
+            "src": "icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        }
+    ],
+    "start_url": "index.html",
+    "display": "standalone",
+    "background_color": "#ffffff",
+    "theme_color": "#165DFF",
+    "orientation": "portrait"
+}
